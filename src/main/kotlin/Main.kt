c8b23d571ebc1b7bd372c8136694b09109afdb7d
Kame-Bashuna <basshunakamrroba@gmail.com>
2024-02-07 23:42:49 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val name = "Yara Bashuna"
+    println(name)
+    var age = 35
+    println(age)
+    var number = 25472347542135
+    println(number)
+    var weight = 57
+    println(weight)
+    var nationality = "Ethiopian"
+    println(nationality)
+
+
+
+}
